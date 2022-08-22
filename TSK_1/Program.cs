7cb6407952434d5796631f6d58ab799b79736079
kksusa <kksusa1@gmail.com>
2022-08-22 15:41:45 +0500

    Задание 1

--- TSK_1/Program.cs
+++ TSK_1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long powerAtoB(int A,int B)
+{
+    long result = 1;
+    for (int i = 1; i <= B; i++) result *= A;
+    return result;
+}
+bool check1 = true;
+bool check2 = true;
+
+while (check1)
+{
+    Console.WriteLine("Введите целое число А:");
+    if (int.TryParse(Console.ReadLine()!, out int number1))
+    {
+        check1 = false;
+        while (check2)
+        {
+        Console.WriteLine("Введите целое число В:");
+        if (int.TryParse(Console.ReadLine()!, out int number2))
+        {
+            if (number2 < 0) Console.WriteLine("Число В введено неправильно.");
+            else
+            {
+                check2 = false;
+                Console.WriteLine("А в степени В равно " + powerAtoB(number1,number2) + ".");
+            } 
+        }
+        else Console.WriteLine("Число В введено неправильно.");
+        }
+    }
+    else Console.WriteLine("Число А введено неправильно.");
+}
