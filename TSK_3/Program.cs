5dbaf628457b0531e220bceae7f7dcdfa7a24f15
kksusa <kksusa1@gmail.com>
2022-08-22 15:42:08 +0500

    Задание 3

--- TSK_3/Program.cs
+++ TSK_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Array()
+{
+    Random rndm = new Random();
+    int [] array = new int[8];
+    Console.Write("[");
+    for (int i = 0;i < array.Length;i++)
+    {
+        array[i] = rndm.Next(1,101);
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }    
+}
+
+Console.WriteLine("Массив из 8 случайных чисел от 1 до 100:");
+Array();
