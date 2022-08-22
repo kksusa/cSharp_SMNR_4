f1576aaf1332b4142ea518d10ae62e25ca318dc3
kksusa <kksusa1@gmail.com>
2022-08-22 15:41:59 +0500

    Задание 2

--- TSK_2/Program.cs
+++ TSK_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int digitsSum(int number)
+{
+    int sum = number % 10;
+    int i = 10;
+    while (number / i != 0)
+    {
+        sum = sum + number / i % 10;
+        i *= 10;
+    }
+    return sum;
+}
+
+bool check1 = true;
+while (check1)
+{
+    Console.WriteLine("Введите целое число:");
+    if (int.TryParse(Console.ReadLine()!, out int number))
+    {
+        if (number < 0) Console.WriteLine("Число введено неправильно.");
+        else
+        {
+            Console.WriteLine("Сумма цифр в этом числе равна " + digitsSum(number) + ".");
+            check1 = false;
+        }    
+    }
+    else Console.WriteLine("Число введено неправильно.");
+}
