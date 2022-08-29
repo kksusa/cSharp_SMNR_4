ec99951f9a7f54834b9d3f75b59a74a73b199a90
Kirill Kirillov <kksusa1@gmail.com>
2022-08-29 13:53:36 +0500

    Добавление функции ввода параметров A и B

--- TSK_1/Program.cs
+++ TSK_1/Program.cs
@@ -1,32 +1,24 @@
-﻿long powerAtoB(int A,int B)
+int CheckNumbers(string param)
+{
+    while (true)
+    {
+        Console.Write($"Введите целое число {param}: ");
+        if (int.TryParse(Console.ReadLine()!, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Число введено неправильно.");
+        }
+    }
+}
+
+long powerAtoB(int A,int B)
 {
     long result = 1;
     for (int i = 1; i <= B; i++) result *= A;
     return result;
 }
-bool check1 = true;
-bool check2 = true;
 
-while (check1)
-{
-    Console.WriteLine("Введите целое число А:");
-    if (int.TryParse(Console.ReadLine()!, out int number1))
-    {
-        check1 = false;
-        while (check2)
-        {
-        Console.WriteLine("Введите целое число В:");
-        if (int.TryParse(Console.ReadLine()!, out int number2))
-        {
-            if (number2 < 0) Console.WriteLine("Число В введено неправильно.");
-            else
-            {
-                check2 = false;
-                Console.WriteLine("А в степени В равно " + powerAtoB(number1,number2) + ".");
-            } 
-        }
-        else Console.WriteLine("Число В введено неправильно.");
-        }
-    }
-    else Console.WriteLine("Число А введено неправильно.");
-}
+Console.WriteLine("А в степени В равно " + powerAtoB(CheckNumbers("A"),CheckNumbers("B")) + ".");
