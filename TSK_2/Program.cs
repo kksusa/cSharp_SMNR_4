d4d6f30273d74bb431ad1b1cb92baf6e4eabb7ef
kksusa <kksusa1@gmail.com>
2022-08-29 13:35:35 +0500

    Изменение функции digitsSum

--- TSK_2/Program.cs
+++ TSK_2/Program.cs
@@ -1,11 +1,10 @@
 ﻿int digitsSum(int number)
 {
-    int sum = number % 10;
-    int i = 10;
-    while (number / i != 0)
+    int sum = 0;
+    while (number > 0)
     {
-        sum = sum + number / i % 10;
-        i *= 10;
+        sum += number % 10;
+        number /= 10;
     }
     return sum;
 }
@@ -21,7 +20,7 @@ while (check1)
         {
             Console.WriteLine("Сумма цифр в этом числе равна " + digitsSum(number) + ".");
             check1 = false;
-        }    
+        }
     }
     else Console.WriteLine("Число введено неправильно.");
 }
